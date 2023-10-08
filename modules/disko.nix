466a3f29c895d8c641006c1db892f6e720267859
Steven <specCon18@gmail.com>
2023-10-07 22:44:52 -0400

    fixed missing disko.devices

--- modules/disko.nix
+++ modules/disko.nix
@@ -1,89 +1,91 @@
 { disks ? [ "/dev/sda" ], ... }: {
-  disk = {
-    disk-0 = {
-      type = "disk";
-      device = builtins.elemAt disks 0;
-      content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
-          {
-            type = "partition";
-            name = "ESP";
-            start = "1MiB";
-            end = "2g";
-            bootable = true;
+  disko.devices = {
+    disk = {
+      disk-0 = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              type = "partition";
+              name = "ESP";
+              start = "1MiB";
+              end = "2g";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            }
+            {
+              name = "swap";
+              type = "partition";
+              start = "2G";
+              end = "26G";
+              part-type = "primary";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            }
+            {
+              type = "partition";
+              name = "luks";
+              start = "40G";
+              end = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ "--allow-discards" ];
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            type = "lvm_lv";
+            size = "256G";
             content = {
               type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+              format = "ext4";
+              mountpoint = "/";
               mountOptions = [
                 "defaults"
               ];
             };
-          }
-          {
-            name = "swap";
-            type = "partition";
-            start = "2G";
-            end = "26G";
-            part-type = "primary";
-            content = {
-              type = "swap";
-              randomEncryption = true;
-            };
-          }
-          {
-            type = "partition";
-            name = "luks";
-            start = "40G";
-            end = "100%";
-            content = {
-              type = "luks";
-              name = "crypted";
-              extraOpenArgs = [ "--allow-discards" ];
-              content = {
-                type = "lvm_pv";
-                vg = "pool";
-              };
-            };
-          }
-        ];
-      };
-    };
-  };
-  lvm_vg = {
-    pool = {
-      type = "lvm_vg";
-      lvs = {
-        root = {
-          type = "lvm_lv";
-          size = "256G";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-            mountOptions = [
-              "defaults"
-            ];
           };
-        };
-        home = {
-          type = "lvm_lv";
-          size = "40G";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/home";
+          home = {
+            type = "lvm_lv";
+            size = "40G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
+            };
           };
-        };
-        media = {
-          type = "lvm_lv";
-          size = "200G";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/mnt/shares/media";
+          media = {
+            type = "lvm_lv";
+            size = "200G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/mnt/shares/media";
+            };
           };
         };
       };
