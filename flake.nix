ae4041461632dffe4a743aedc702aca18d53d701
Steven <specCon18@gmail.com>
2023-10-08 20:16:44 -0400

    added surreal precompiled src

--- flake.nix
+++ flake.nix
@@ -2,13 +2,23 @@
   description = "NixOS configuration for Proxmox";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    # The unfree instance
+    inputs.nixpkgs-unfree = {
+      url = "github:numtide/nixpkgs-unfree";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Optionally, pull pre-built binaries from this project's cache
     disko = {
       url = "github:nix-community/disko/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  nixConfig ={
+    extra-substituters = [ "https://numtide.cachix.org" ];
+    extra-trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
+  };
 
-  outputs = { self, nixpkgs, disko, ... }: {
+  outputs = { self, nixpkgs, disko, nixpkgs-unfree ... }: {
     apps.x86_64-linux.disko = {
       type = "app";
       program = "${disko.packages.x86_64-linux.disko}/bin/disko";
--- modules/surrealdb.nix
+++ modules/surrealdb.nix
@@ -1,7 +1,7 @@
-{config, lib, pkgs, modulesPath, ... }:
+{config, lib, nixpkgs-unfree, modulesPath, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    surrealdb
+  environment.systemPackages = [
+    nixpkgs-unfree.packages.x86_64-linux.surrealdb
   ];
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "surrealdb" ];
 }
